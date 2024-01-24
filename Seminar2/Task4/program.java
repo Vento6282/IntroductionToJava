cd10d4f3c0d3db15ce0526d554208e4e234b1e54
AndreevAV <koriolanrus@gmail.com>
2024-01-24 17:30:48 +0300

    Added Seminar2 Task4

--- Seminar2/Task4/program.java
+++ Seminar2/Task4/program.java
@@ -6,6 +6,8 @@ package IntroductionToJava.Seminar2.Task4;
 
 public class program {
     public static void main(String[] args) {
-        
+        String pathProject = System.getProperty("user.dir");
+        String pathFile = pathProject.concat("\file.txt");
+        System.out.println(pathProject + "        " + pathFile);
     }
 }
